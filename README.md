1a51975c9d387ca9739a78fa453d351b95ef2245
Cyrill Lippuner <cyrill.lippuner@aposphere.io>
2019-06-25 14:52:01 +0200

    Shutdown script

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # iot-pi-cam
+
+## Install hardware shutdown
+
+Make script executable with `chmod +x /home/pi/iot-pi-cam/shutdown.py`.
+
+Create a service in `/lib/systemd/system/` (as root) with name "switchoff.service" and content:
+
+```
+  [Unit]
+  Description=Shutdown Rasperry With Button
+  After=multi-user.target
+
+  [Service]
+  Type=idle
+  ExecStart=/home/pi/iot-pi-cam/shutdown.py &
+
+  [Install]
+  WantedBy=multi-user.target
+```
+Make service executable `sudo chmod 644 /lib/systemd/system/switchoff.service`, register it and activate it:
+```
+  sudo systemctl daemon-reload
+  sudo systemctl enable switchoff.service
+  sudo systemctl start switchoff.service
+```
+After restart:
+```
+  sudo systemctl status switchoff.service
+```
+Use `disable` to turn it off.
+
+## Install tools
+
+Docker: https://sebastianbrosch.blog/docker-auf-einem-raspberry-pi-3-installieren/
+To avoid using root access: `sudo usermod -a -G docker pi`
+Git
---
+++ shutdown.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# shutdown/reboot Raspberry Pi mittels Taste
+
+"""Control hardware shutdown behaviour."""
+
+import RPi.GPIO as GPIO
+import subprocess, time, sys, syslog, os
+
+# GPIO-Port, an dem die Taste gegen GND angeschlossen ist
+# GPIO 5, Pin 29 (GND waere daneben auf Pin 30)
+PORT = 5
+
+# Schwelle fuer Shutdown (in Sekunden), wird die Taste kuerzer
+# gedruckt, erfolgt ein Reboot
+T_SHUT = 3
+
+# Entprellzeit fuer die Taste
+T_PRELL = 0.05
+
+uid = os.getuid()
+if uid > 0:
+    print ("Programm benoetigt root-Rechte!")
+    sys.exit(0)
+
+# GPIO initialisieren, BMC-Pinnummer, Pullup-Widerstand
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PORT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Zeitdauer des Tastendrucks
+duration = 0
+
+# Interrupt-Routine fuer die Taste
+def buttonISR(pin):
+    """Interrupt routing listening to button."""
+    global duration
+
+    if not (GPIO.input(pin)):
+        # Taste gedrueckt
+        if duration == 0:
+            duration = time.time()
+        else:
+        # Taste losgelassen
+            if duration > 0:
+                elapsed = (time.time() - duration)
+                duration = 0
+                if elapsed >= T_SHUT:
+                    syslog.syslog('Shutdown: System halted');
+                    subprocess.call(['shutdown', '-h', 'now'], shell=False)
+                elif elapsed >= T_PRELL:
+                    syslog.syslog('Shutdown: System rebooted');
+                    subprocess.call(['shutdown', '-r', 'now'], shell=False)
+
+# Interrupt fuer die Taste einschalten
+GPIO.add_event_detect(PORT, GPIO.BOTH, callback=buttonISR)
+
+syslog.syslog('Shutdown.py started');
+while True:
+    try:
+        time.sleep(300)
+    except KeyboardInterrupt:
+        syslog.syslog('Shutdown terminated (Keyboard)');
+        print ("Bye")
+        sys.exit(0)
